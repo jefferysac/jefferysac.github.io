987f58cb79ec3dc3b29def98f08fde84fbf0ee69
jefferysac <jeffery.sac@gmail.com>
2019-08-21 19:51:56 +0800

    Webpack works , let it be more modern.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import _ from 'lodash';
 
+const idJson = require('./id.json');
+
+function addId(l){
+    let idA = document.createElement("a");
+    idA.setAttribute("href","#");
+    let content = document.createTextNode(idJson.platform[l]);
+    idA.appendChild(content);
+    let idLine = document.createElement("li");
+    idLine.appendChild(idA);
+    return idLine;
+}
+
 function title(){
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' there');
+    element.innerHTML = _.join(['Hello','here'], '');
     return element;
 }
 
 document.body.appendChild(title());
+for(let i in idJson.platform){
+        document.body.appendChild(addId(i));
+    }   
+
+document.getElementById('drink').appendChild(addId(1));
