094e7e5c1800f94b5eb5928e8661511ab3934065
jefferysac <jeffery.sac@gmail.com>
2019-09-04 21:42:09 +0800

    add sass webpack loader here

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import '../src/style.css';
+import '../src/styles/scss/main.scss';
 
 const idJson = require('./id.json');
 console.log('This is index JS')
@@ -7,23 +8,25 @@ console.log('This is index JS')
 function addId(l){
     let idA = document.createElement("a");
     idA.setAttribute("href","#");
+
     let content = document.createTextNode(idJson.platform[l]);
     idA.appendChild(content);
+
     let idLine = document.createElement("li");
     idLine.appendChild(idA);
     return idLine;
 }
 
 function title(){
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','here'], '');
+    const element = document.createElement('h1');
+    element.innerHTML = "This is message";
     element.classList.add('body');
     return element;
 }
 
 document.body.appendChild(title());
+
 for(let i in idJson.platform){
         document.body.appendChild(addId(i));
     }   
 
-document.getElementById('drink').appendChild(addId(1));
--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,11 @@ module.exports = {
     module:{
         rules:[
             {
-                test:/\.css$/,
+                test:[/.css$|.scss$/],
                 use:[
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
+                    'sass-loader'
                 ] 
             }
         ]
