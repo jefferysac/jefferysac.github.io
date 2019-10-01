cd0801cf246c2e3d732461804567731cb64cb5b5
jefferysac <jeffery.sac@gmail.com>
2019-10-01 17:09:03 +0800

    Use webpack 4 sane path feature

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path =require('path');
 
 module.exports = {
-    entry:['./src/index.js'],
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'index.js'
-    },
     plugins:[
         new HtmlWebpackPlugin(
             {
